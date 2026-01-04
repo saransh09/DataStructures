329449d1a7d386cf80d2f0ef0cc233ebc9ebc56b
Saransh Agarwal <saransh.agarwal@gmail.com>
2026-01-04 23:25:02 +0100

    Adding MakeUnique implementation

---
+++ C++/MakeUnique/include/make_unique.hpp
@@ -0,0 +1,11 @@
+#include <memory>
+#include <utility>
+
+namespace ds {
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args &&...args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+} // namespace ds
